e0c76abacd70792275b248df0a69108969431b6a
yuliu529 <55214270+yuliu529@users.noreply.github.com>
2019-09-29 19:44:31 -0400

    Delete API_Keys.py

--- API_Keys.py
+++
@@ -1,7 +0,0 @@
-def API_Keys():
-    file = open('/home/jsneal/Desktop/API_Keys/Twitter/api.txt', 'r') # The file where my API keys were stored.
-    api_key = file.readline()[0:-1]
-    api_secret_key = file.readline()[0:-1]
-    access_token = file.readline()[0:-1]
-    access_token_secret = file.readline()
-    return [api_key, api_secret_key, access_token, access_token_secret]
